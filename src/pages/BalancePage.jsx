71fcfb234264945678bfabdd1117d405ae67494f
Leonardo Tolaba <leonardotolaba.20@gmail.com>
2024-04-09 16:46:48 -0300

    Arreglando el diseño

--- src/pages/BalancePage.jsx
+++ src/pages/BalancePage.jsx
@@ -16,28 +16,13 @@ const BalancePage = () => {
   return (
     <div className="w-full h-[65vh] flex flex-col items-center justify-center text-text">
       <h1 className="text-text text-2xl py-4">Balance</h1>
-      <h2 className="text-text text-xl py-4">
-        Salarios totales:{" "}
-        <span
-          className={`${
-            totalSalaries < totalExpenses
-              ? " text-red-500 border-b border-red-400"
-              : "text-cyan-500"
-          }`}
-        >
-          ${formatNumber(totalSalaries)}
-        </span>
-      </h2>
-      <h2 className="text-text text-xl py-4">
-        Gastos totales:{" "}
-        <span className="text-cyan-300">${formatNumber(totalExpenses)}</span>
-      </h2>
+
       <div className="w-[70%] flex flex-col items-center justify-around border border-yellow-200 py-4">
         <h2 className="text-text text-xl">Porcentaje por usuario:</h2>
         {usersWithPercentage && totalExpenses < totalSalaries ? (
           usersWithPercentage.map((user) => (
             <div
-              className="w-full flex items-center justify-around"
+              className="w-full flex items-center justify-around my-1"
               key={user.id}
             >
               <p>
@@ -58,15 +43,15 @@ const BalancePage = () => {
               <p>
                 Monto: ${" "}
                 <span className="text-green-500 border-b border-b-green-400  font-bold">
-                  {formatNumber(((user.percentage * 1) / 100).toFixed(4) * totalExpenses)}
+                  {formatNumber(
+                    ((user.percentage * 1) / 100).toFixed(4) * totalExpenses
+                  )}
                 </span>{" "}
                 <span className="text-cyan-300"> ({user.name})</span>
               </p>
-              <button
-            className="bg-buttons  hover:bg-buttons-hover text-buttons-text font-bold py-2 px-4 border-b-4 border-b-buttons-border hover:border-buttons-hover rounded"
-          >
-            +
-          </button>
+              <button className="bg-buttons  hover:bg-buttons-hover text-buttons-text font-bold p-2 border-b-4 border-b-buttons-border hover:border-buttons-hover rounded">
+                Agregar adicional
+              </button>
             </div>
           ))
         ) : (
@@ -76,6 +61,22 @@ const BalancePage = () => {
           </p>
         )}
       </div>
+      <h2 className="text-text text-xl py-4">
+        Salarios totales:{" "}
+        <span
+          className={`${
+            totalSalaries < totalExpenses
+              ? " text-red-500 border-b border-red-400"
+              : "text-cyan-500"
+          }`}
+        >
+          ${formatNumber(totalSalaries)}
+        </span>
+      </h2>
+      <h2 className="text-text text-xl py-4">
+        Gastos totales:{" "}
+        <span className="text-cyan-300">${formatNumber(totalExpenses)}</span>
+      </h2>
     </div>
   );
 };
